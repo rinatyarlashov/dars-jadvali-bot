a13fdf0fc739d7cf7635058e7f449d002714a257
rinatyarlashov <rinatyarlashov@gmail.com>
2026-02-24 12:30:40 +0500

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,20 +1,13 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE IF NOT EXISTS teachers (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  full_name TEXT UNIQUE NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS schedules (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  teacher_id INTEGER NOT NULL,
-  day TEXT NOT NULL,          -- Dushanba, Seshanba...
-  start_time TEXT NOT NULL,   -- 08:30
-  end_time TEXT NOT NULL,     -- 09:50
-  subject TEXT NOT NULL,
   direction TEXT,
   course TEXT,
   group_name TEXT,
-  room TEXT,
-  FOREIGN KEY (teacher_id) REFERENCES teachers(id) ON DELETE CASCADE
+  day TEXT,
+  lesson_num INTEGER,
+  subject TEXT,
+  teacher TEXT,
+  room TEXT
 );
